7da6f484b00c61f51319dc9159e6fccd2e68ff85
Andreas Persson <151462464+infotillandreas@users.noreply.github.com>
2025-05-28 10:33:36 +0200

    Added layout to details table, including spacing and placeholder icons

--- src/components/main-card-details-list.tsx
+++ src/components/main-card-details-list.tsx
@@ -3,6 +3,7 @@
 
 
 import React from "react";
+import "./main-card-details-list.css";
 
 
 const MainCardDetails: React.FC = () => {
@@ -10,36 +11,39 @@ const MainCardDetails: React.FC = () => {
         <>
             <div className="main-card-details-container">
                 <table className="main-card-details">
-                    <tr className="sunrise">
-                        <td><img src="#" alt="sunrise icon"></img></td>
-                        <td><p>Sunrise</p></td>
-                        <td><p>05:45</p></td>
-                    </tr>
-                    <tr className="sunset">
-                        <td><img src="#" alt="sunset icon"></img></td>
-                        <td><p>Sunset</p></td>
-                        <td><p>23:30</p></td>
-                    </tr>
-                    <tr className="precipitation">
-                        <td><img src="#" alt="umbrella icon"></img></td>
-                        <td><p>Precipitation</p></td>
-                        <td><p>0.02 mm</p></td>
-                    </tr>
-                    <tr className="wind">
-                        <td><img src="#" alt="wind icon"></img></td>
-                        <td><p>Wind</p></td>
-                        <td><p>12 m/s</p></td>
-                    </tr>
-                    <tr className="humidity">
-                        <td><img src="#" alt="humidity icon"></img></td>
-                        <td><p>Humidity</p></td>
-                        <td><p>74%</p></td>
-                    </tr>
-                    <tr className="uv-index">
-                        <td><img src="#" alt="uv-index icon"></img></td>
-                        <td><p>UV Index</p></td>
-                        <td><p>4</p></td>
-                    </tr>
+                    <tbody>
+                        <tr className="sunrise">
+                            <td><img src="/src/assets/react.svg" alt="sunrise icon"></img></td>
+                            <td><p>Sunrise</p></td>
+                            <td><p>05:45</p></td>
+                        </tr>
+                        <tr className="sunset">
+                            <td><img src="/src/assets/react.svg" alt="sunset icon"></img></td>
+                            <td><p>Sunset</p></td>
+                            <td><p>23:30</p></td>
+                        </tr>
+                        <tr className="precipitation">
+                            <td><img src="/src/assets/react.svg" alt="umbrella icon"></img></td>
+                            <td><p>Precipitation</p></td>
+                            <td><p>0.02 mm</p></td>
+                        </tr>
+                        <tr className="wind">
+                            <td><img src="/src/assets/react.svg" alt="wind icon"></img></td>
+                            <td><p>Wind</p></td>
+                            <td><p>12 m/s</p></td>
+                        </tr>
+                        <tr className="humidity">
+                            <td><img src="/src/assets/react.svg" alt="humidity icon"></img></td>
+                            <td><p>Humidity</p></td>
+                            <td><p>74%</p></td>
+                        </tr>
+                        <tr className="uv-index">
+                            <td><img src="/src/assets/react.svg" alt="uv-index icon"></img></td>
+                            <td><p>UV Index</p></td>
+                            <td><p>4</p></td>
+                        </tr>
+                    </tbody>
+                    
 
                 </table>
 
