2e7065a8c01a297882b60ceb519cd274af198438
MalikTallouzi <maliktallouzi@hotmail.com>
2025-05-28 15:20:25 +0200

    Carousel works
    Carousel created with Embla-carousel. Works as expected.

--- src/components/main-card-carousel.tsx
+++ src/components/main-card-carousel.tsx
@@ -2,28 +2,64 @@
 
 
 import React from "react";
-import HourlyCard from "./hourly-card";
+import useEmblaCarousel from 'embla-carousel-react';
 import WeeklyCard from "./weekly-card";
 
 import "./main-card-carousel.css";
 
 
 const MainCardCarousel: React.FC = () => {
+    const [emblaRef] = useEmblaCarousel({
+        align: 'start',
+        containScroll: 'trimSnaps',
+        dragFree: true
+    });
+
     return (
-        <>
-            <div className="main-card-carousel-container">
-                <h3>Title</h3>
-                <div className="carousel-test">
-                    <WeeklyCard />
-                    <WeeklyCard />
-                    <WeeklyCard />
-                    <WeeklyCard />
-
+        <div className="main-card-carousel-container">
+            <h3>Title</h3>
+            <div className="embla" ref={emblaRef}>
+                <div className="embla__container">
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                     <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                     <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
+                    <div className="embla__slide">
+                        <WeeklyCard />
+                    </div>
                 </div>
-
             </div>
-        </>
+        </div>
     );
 }
 
-export default MainCardCarousel
+export default MainCardCarousel;
