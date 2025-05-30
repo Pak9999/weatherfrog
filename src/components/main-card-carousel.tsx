6d4166548efc668a7e10d2813e8f946d90d942c8
MalikTallouzi <maliktallouzi@hotmail.com>
2025-05-30 11:32:38 +0200

    Favorites and recent carousel now works as intended. Hourly and weekly also works fine. Though, I need to adjust medium-cards for favorites and recent.

--- src/components/main-card-carousel.tsx
+++ src/components/main-card-carousel.tsx
@@ -18,106 +18,144 @@ const MainCardCarousel: React.FC<MainCardCarouselProps> = ({ forecastType }) => 
     const [emblaRef] = useEmblaCarousel({
         align: 'start',
         containScroll: 'trimSnaps',
-        dragFree: true
+        dragFree: true,
+        loop: false
     });
 
     return (
-        <>
-            <div className="main-card-carousel-container">
-                <h3>{forecastType === "hourly" ? "Hourly" : "Weekly"}</h3>
-                <div className="embla" ref={emblaRef}>
-                    <div className="embla__container">
-                        <div className="embla__slide">
-                            {forecastType === "hourly" ? (
-                                <>
-                                    <HourlyCard
+        <div className="main-card-carousel-container">
+            <h3>{forecastType === "hourly" ? "Hourly" : "Weekly"}</h3>
+            <div className="embla" ref={emblaRef}>
+                <div className="embla__container">
+                    {forecastType === "hourly" ? (
+                        <>
+                            <div className="embla__slide">
+                                <HourlyCard
                                     hour="14:00"
                                     weatherIcon="/src/assets/react.svg"
                                     temperature="20°C"
-                                    />
-                                    <HourlyCard
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
+                                    hour="15:00"
+                                    weatherIcon="/src/assets/react.svg"
+                                    temperature="22°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
+                                    hour="16:00"
+                                    weatherIcon="/src/assets/react.svg"
+                                    temperature="21°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
+                                    hour="17:00"
+                                    weatherIcon="/src/assets/react.svg"
+                                    temperature="19°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
                                     hour="14:00"
                                     weatherIcon="/src/assets/react.svg"
                                     temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
+                                    hour="15:00"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    temperature="22°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
+                                    hour="16:00"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    temperature="21°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <HourlyCard
+                                    hour="17:00"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    temperature="19°C"
+                                />
+                            </div>
+                        </>
+                    ) : ( 
+                        <>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Mon"
+                                    date="15/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    maxTemp="25°C"
+                                    minTemp="15°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Tue"
+                                    date="16/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    maxTemp="23°C"
+                                    minTemp="14°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Wed"
+                                    date="17/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    maxTemp="24°C"
+                                    minTemp="16°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Thu"
+                                    date="18/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    maxTemp="25°C"
+                                    minTemp="15°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Fri"
+                                    date="19/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    maxTemp="23°C"
+                                    minTemp="14°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Sat"
+                                    date="20/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
+                                    maxTemp="24°C"
+                                    minTemp="16°C"
+                                />
+                            </div>
+                            <div className="embla__slide">
+                                <WeeklyCard 
+                                    day="Sun"
+                                    date="21/02"
                                     weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
-                                    weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                    <HourlyCard
-                                    hour="14:00"
-                                    weatherIcon="/src/assets/react.svg"
-                                    temperature="20°C"
-                                    />
-                                </>
-                            ) : ( 
-                                <>
-                                    <WeeklyCard /* Update props */
-                                        day="Mon"
-                                        date="15/02"
-                                        weatherIcon="/src/assets/react.svg"
-                                        maxTemp="25°C"
-                                        minTemp="15°C"
-                                    />
-                                </>
-                            )
-                            }
-                        </div>
-                    </div>
+                                    maxTemp="24°C"
+                                    minTemp="16°C"
+                                />
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/components/medium-card-carousel.tsx
+++ src/components/medium-card-carousel.tsx
@@ -1,8 +1,6 @@
-
-
 import React from "react";
+import useEmblaCarousel from 'embla-carousel-react';
 import "./medium-card-carousel.css";
-
 import MediumCard from "./medium-card";
 
 
@@ -11,20 +9,98 @@ interface MediumCardCarouselProps {
 }
 
 const MediumCardCarousel: React.FC<MediumCardCarouselProps> = ({ carouselType }) => {
+    const [emblaRef] = useEmblaCarousel({
+            align: 'start',
+            containScroll: 'trimSnaps',
+            dragFree: true,
+            loop: false
+        });
+    
     return (
         <div className="medium-card-carousel-container">
             <h3>{carouselType === "favorites" ? "Favorites" : "Recent"}</h3>
-            <div className="medium-card-carousel">
-                <MediumCard 
-                locationName="Eslöv"
-                city="Sweden"
-                temperature="-20"
-                weatherIcon="/src/assets/react.svg"
-                maxTemp="25"
-                minTemp="15"
-                precipitation="12"
-                wind="5"
-                />
+            <div className="medium-embla" ref={emblaRef}>
+                <div className="medium-embla__container">
+                    {carouselType === "favorites" ? (
+                        <>
+                            <div className="medium-embla__slide">
+                                <MediumCard
+                                    locationName="Eslöv"
+                                    city="Sweden"
+                                    temperature="20"
+                                    weatherIcon="/src/assets/react.svg"
+                                    maxTemp="25"
+                                    minTemp="15"
+                                    precipitation="12"
+                                    wind="5"
+                                />
+                            </div>
+                            <div className="medium-embla__slide">
+                                <MediumCard
+                                    locationName="Eslöv"
+                                    city="Sweden"
+                                    temperature="20"
+                                    weatherIcon="/src/assets/react.svg"
+                                    maxTemp="25"
+                                    minTemp="15"
+                                    precipitation="12"
+                                    wind="5"
+                                />
+                            </div> 
+                            <div className="medium-embla__slide">
+                                <MediumCard
+                                    locationName="Eslöv"
+                                    city="Sweden"
+                                    temperature="20"
+                                    weatherIcon="/src/assets/react.svg"
+                                    maxTemp="25"
+                                    minTemp="15"
+                                    precipitation="12"
+                                    wind="5"
+                                />
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="medium-embla__slide">
+                                <MediumCard
+                                    locationName="Eslöv"
+                                    city="Sweden"
+                                    temperature="20"
+                                    weatherIcon="/src/assets/react.svg"
+                                    maxTemp="25"
+                                    minTemp="15"
+                                    precipitation="12"
+                                    wind="5"
+                                />
+                            </div>
+                            <div className="medium-embla__slide">
+                                <MediumCard
+                                    locationName="Eslöv"
+                                    city="Sweden"
+                                    temperature="20"
+                                    weatherIcon="/src/assets/react.svg"
+                                    maxTemp="25"
+                                    minTemp="15"
+                                    precipitation="12"
+                                    wind="5"
+                                />
+                            </div>
+                            <div className="medium-embla__slide">
+                                <MediumCard
+                                    locationName="Eslöv"
+                                    city="Sweden"
+                                    temperature="20"
+                                    weatherIcon="/src/assets/react.svg"
+                                    maxTemp="25"
+                                    minTemp="15"
+                                    precipitation="12"
+                                    wind="5"
+                                />
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         </div>
     );
